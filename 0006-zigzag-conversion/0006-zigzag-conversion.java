3748c74cf87555c94922ffcc08f2009cd7eea603
sumit0991 <supan98122@gmail.com>
2026-07-01 21:34:00 +0530

    Time: 13 ms (12.33%), Space: 48 MB (5.72%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        String ans[]=new String[numRows];
+        for(int i=0;i<numRows;i++){
+            ans[i]="";
+        }
+        int i=0;
+        while(i<s.length()){
+            for(int j=0;j<numRows && i<s.length();j++){
+                ans[j] += s.charAt(i++);
+            }
+            for(int j=numRows-2;j>0 && i<s.length();j--){
+                ans[j] += s.charAt(i++);
+            }
+        }
+        String res="";
+        for(String str : ans){
+            res += str;
+        }
+        return res;
+    }
+}
