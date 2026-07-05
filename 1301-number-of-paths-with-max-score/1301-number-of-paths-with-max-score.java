4d2849ffe0ae79998b5b23502335481d7a499ec6
sumit0991 <supan98122@gmail.com>
2026-07-05 20:22:21 +0530

    Time: 22 ms (22.35%), Space: 44.2 MB (97.65%) - LeetHub

---
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.java
@@ -0,0 +1,78 @@
+class Solution {
+    public int[] pathsWithMaxScore(List<String> board) {
+
+        int n = board.size();
+        int MOD = 1_000_000_007;
+
+        int[][] score = new int[n][n];
+        int[][] ways = new int[n][n];
+
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(score[i], -1);
+        }
+
+        score[n - 1][n - 1] = 0;
+        ways[n - 1][n - 1] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                char c = board.get(i).charAt(j);
+
+                if (c == 'X')
+                    continue;
+
+                if (i == n - 1 && j == n - 1)
+                    continue;
+
+                int best = -1;
+                int count = 0;
+
+        
+                if (i + 1 < n && score[i + 1][j] != -1) {
+                    if (score[i + 1][j] > best) {
+                        best = score[i + 1][j];
+                        count = ways[i + 1][j];
+                    } else if (score[i + 1][j] == best) {
+                        count = (count + ways[i + 1][j]) % MOD;
+                    }
+                }
+
+                if (j + 1 < n && score[i][j + 1] != -1) {
+                    if (score[i][j + 1] > best) {
+                        best = score[i][j + 1];
+                        count = ways[i][j + 1];
+                    } else if (score[i][j + 1] == best) {
+                        count = (count + ways[i][j + 1]) % MOD;
+                    }
+                }
+
+            
+                if (i + 1 < n && j + 1 < n && score[i + 1][j + 1] != -1) {
+                    if (score[i + 1][j + 1] > best) {
+                        best = score[i + 1][j + 1];
+                        count = ways[i + 1][j + 1];
+                    } else if (score[i + 1][j + 1] == best) {
+                        count = (count + ways[i + 1][j + 1]) % MOD;
+                    }
+                }
+
+                if (best == -1)
+                    continue;
+
+                int value = 0;
+                if (c != 'E' && c != 'S')
+                    value = c - '0';
+
+                score[i][j] = best + value;
+                ways[i][j] = count;
+            }
+        }
+
+        if (ways[0][0] == 0)
+            return new int[]{0, 0};
+
+        return new int[]{score[0][0], ways[0][0]};
+    }
+}
