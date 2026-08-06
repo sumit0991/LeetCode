1380b917e19cf59aaa5ae1a9d909d0a43d945fc9
Sumit Pandey <supan98122@gmail.com>
2026-08-06 10:03:18 +0530

    Time: 50 ms (95.45%), Space: 282.1 MB (65.45%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : invocations) {
+            graph[edge[0]].add(edge[1]);
+        }
+
+        boolean[] suspicious = new boolean[n];
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(k);
+        suspicious[k] = true;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            for (int next : graph[cur]) {
+                if (!suspicious[next]) {
+                    suspicious[next] = true;
+                    q.offer(next);
+                }
+            }
+        }
+
+        for (int[] edge : invocations) {
+            if (!suspicious[edge[0]] && suspicious[edge[1]]) {
+                List<Integer> ans = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    ans.add(i);
+                }
+                return ans;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
